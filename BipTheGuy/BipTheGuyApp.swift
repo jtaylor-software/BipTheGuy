34f1cfb8ad8092836a4d875077304e6c0208de7f
Jeremy Taylor <jtaylor@jctaylor.org>
2025-04-29 08:15:30 -0500

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Jeremy Taylor on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
