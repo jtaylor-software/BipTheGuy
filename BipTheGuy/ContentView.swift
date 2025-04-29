197d80a3519874f0954d449061e5f9233134abc6
Jeremy Taylor <jtaylor@jctaylor.org>
2025-04-29 08:32:03 -0500

    Initial UI setup

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,17 +6,48 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+  @State private var audioPlayer: AVAudioPlayer!
+  
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Spacer()
+            Image("clown")
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+              playsound(soundName: "punchSound")
+            }
+              
+          Spacer()
+          
+          Button {
+            
+          } label: {
+            Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+          }
+
         }
         .padding()
     }
+  
+  func playsound(soundName: String) {
+    if audioPlayer != nil && audioPlayer.isPlaying {
+      audioPlayer.stop()
+    }
+    guard let soundFile = NSDataAsset(name: soundName) else {
+      print("😡 Could not read file named \(soundName)")
+      return
+    }
+    do {
+      audioPlayer = try AVAudioPlayer(data: soundFile.data)
+      audioPlayer.play()
+    } catch {
+      print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+    }
+  }
 }
 
 #Preview {
