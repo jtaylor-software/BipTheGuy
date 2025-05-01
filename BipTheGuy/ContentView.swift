f4eb4bf84235121fde4994924d1314a5173f518f
Jeremy Taylor <jtaylor@jctaylor.org>
2025-04-30 23:25:05 -0500

    Add code that allows picking our own image to Bip

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -7,15 +7,18 @@
 
 import SwiftUI
 import AVFAudio
+import PhotosUI
 
 struct ContentView: View {
   @State private var audioPlayer: AVAudioPlayer!
+  @State private var selectedPhoto: PhotosPickerItem?
   @State private var isFullSize = true
+  @State private var bipImage = Image("clown")
   
   var body: some View {
     VStack {
       Spacer()
-      Image("clown")
+      bipImage
         .resizable()
         .scaledToFit()
         .scaleEffect(isFullSize ? 1.0 : 0.9)
@@ -30,12 +33,18 @@ struct ContentView: View {
       
       Spacer()
       
-      Button {
-        
-      } label: {
+      PhotosPicker(selection: $selectedPhoto, matching: .images, preferredItemEncoding: .automatic) {
         Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
       }
-      
+      .onChange(of: selectedPhoto) {
+        Task {
+          guard let selectedImage = try? await selectedPhoto?.loadTransferable(type: Image.self) else {
+            print( "😡 Could not get image from loadTransferable")
+            return
+          }
+          bipImage = selectedImage
+        }
+      }
     }
     .padding()
   }
